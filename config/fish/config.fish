81f0644658f5f766232c0aed20ed7475ba970825
Peter Andersson <peter.andersson@hemnet.se>
2017-05-12 07:56:23 +0200

    working config.fish (based on my old bash_profile

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,14 @@
-set -xg VISUAL "nvim"
-set -xg EDITOR $VISUAL
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+
+set -x VISUAL "nvim"
+set -x EDITOR $VISUAL
 
 if which rbenv
-  set -xg PATH $HOME/.rbenv/bin
-  eval (rbenv init -)
+  set -x PATH $HOME/.rbenv/shims $PATH
+  rbenv init - | source
+end
+
+if test -d $HOME/hemnet/src/hemnet-terminal-command
+  set -x PATH $HOME/hemnet/src/hemnet-terminal-command/bin $PATH
 end
