721ed342954c0c81c11edec3e744c8439818a8f7
Peter Andersson <pean@wa.se>
2023-03-28 20:53:53 +0200

    removing hemnet stuff from fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,16 +4,9 @@ set -x LANG en_US.UTF-8
 set -x VISUAL "nvim"
 set -x EDITOR $VISUAL
 
-if test -d $HOME/src/hemnet/hemnet-terminal-command
-  set -x PATH $HOME/src/hemnet/hemnet-terminal-command/bin $PATH
-end
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-set -g fish_user_paths "/usr/local/opt/postgresql@9.6/bin" $fish_user_paths
-
-export KP_USER_EMAIL=peter.andersson@hemnet.se
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
 export GPG_TTY=(tty)
 
@@ -29,6 +22,3 @@ source (brew --prefix asdf)/libexec/asdf.fish
 set fish_greeting
 
 starship init fish | source
-
-# for broker integration
-set -g fish_user_paths ".git/safe/../../bin" $fish_user_paths
