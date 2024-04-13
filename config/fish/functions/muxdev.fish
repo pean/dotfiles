818a5bc89dee14a41e149e9348aa9f008f1b179b
Peter Andersson <pean@wa.se>
2024-04-13 22:28:21 +0200

    fix: git repo name not dependent on remote

--- config/fish/functions/muxdev.fish
+++ config/fish/functions/muxdev.fish
@@ -1,6 +1,6 @@
 function muxdev
   if git rev-parse --git-dir >/dev/null 2>&1
-    set reponame (basename -s .git (git config --get remote.origin.url))
+    set reponame (basename (git rev-parse --show-toplevel))
     tmuxinator start -n $reponame dev
   else
     echo "Not a repo"
