aafc8df0c1494dd8f1773f912bf9472ef372caaa
Peter Andersson <pean@wa.se>
2024-08-01 00:10:26 +0200

    fix: strip leading dots from reponame

--- config/fish/functions/muxdev.fish
+++ config/fish/functions/muxdev.fish
@@ -1,6 +1,6 @@
 function muxdev
   if git rev-parse --git-dir >/dev/null 2>&1
-    set reponame (basename (git rev-parse --show-toplevel))
+    set reponame (basename (git rev-parse --show-toplevel) | sed 's/^\.//')
     tmuxinator start -n $reponame dev
   else
     echo "Not a repo"
