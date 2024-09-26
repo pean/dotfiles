f077430536a43f9cecae38d42ad59046a957f20a
Peter Andersson <pean@wa.se>
2024-09-26 08:58:01 +0200

    cleanup: remove some stuff from fish config no longer needed

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,26 +4,12 @@ set -x LANG en_US.UTF-8
 set -x VISUAL "nvim"
 set -x EDITOR $VISUAL
 
-set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
-set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
-set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
-set -g fish_user_paths "/opt/homebrew/opt/make/libexec/gnubin" $fish_user_paths
-set -g fish_user_paths "/Users/peter/src/getdreams/dreams-cli/target/release" $fish_user_paths
-
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-
-export GPG_TTY=(tty)
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
-set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl/lib/pkgconfig"
-
-# source (brew --prefix asdf)/libexec/asdf.fish
-
 set fish_greeting
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+set -g fish_user_paths "/Users/peter/src/getdreams/dreams-cli/target/release" $fish_user_paths
+set -g fish_user_path "$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools" $fish_user_paths
 
 starship init fish | source
 mise activate fish | source
