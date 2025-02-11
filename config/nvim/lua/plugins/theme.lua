d1354459d0e2f88c6f634ae45b8b3e4256aeadd1
Peter Andersson <pean@wa.se>
2025-02-11 14:16:57 +0100

    feat: switch to theme catppuccin everywhere

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -5,4 +5,18 @@ return {
     lazy = true,
     dir = "~/.config/nvim/pack/themes/dracula_pro",
   },
+  { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    init = function()
+      require('catppuccin').setup({
+        flavour = "mocha",
+        integrations = {
+          gitgutter = true,
+
+        }
+      })
+    end
+  },
 }
