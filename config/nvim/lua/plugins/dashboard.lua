a783b3b9741fee10cf3eede056fbfe6d1b2f2a9e
Peter Andersson <pean@wa.se>
2025-04-17 08:51:34 +0200

    vim: disable the start page thing

--- config/nvim/lua/plugins/dashboard.lua
+++ config/nvim/lua/plugins/dashboard.lua
@@ -1,34 +1,34 @@
 return {
-  "goolord/alpha-nvim",
-  config = function()
-    local startify = require("alpha.themes.startify")
-    local stats = require("lazy").stats()
-
-    startify.section.top_buttons.val = {
-      startify.button("e", "New file"),
-      startify.button("o", "New note", ":ObsidianNew<CR>"),
-    }
-
-    startify.section.mru.val = { { type = "padding", val = 0 } }
-    startify.file_icons.enabled = false
-
-    startify.section.footer.val = {
-      {
-        opts = {
-          position = "center",
-        },
-        type = "text",
-        val = "⚡ Neovim loaded "
-          .. stats.loaded
-          .. "/"
-          .. stats.count
-          .. " plugins"
-      }
-    }
-
-    require("alpha").setup(
-      startify.config
-    )
-
-  end,
+  -- "goolord/alpha-nvim",
+  -- config = function()
+  --   local startify = require("alpha.themes.startify")
+  --   local stats = require("lazy").stats()
+  --
+  --   startify.section.top_buttons.val = {
+  --     startify.button("e", "New file"),
+  --     startify.button("o", "New note", ":ObsidianNew<CR>"),
+  --   }
+  --
+  --   startify.section.mru.val = { { type = "padding", val = 0 } }
+  --   startify.file_icons.enabled = false
+  --
+  --   startify.section.footer.val = {
+  --     {
+  --       opts = {
+  --         position = "center",
+  --       },
+  --       type = "text",
+  --       val = "⚡ Neovim loaded "
+  --         .. stats.loaded
+  --         .. "/"
+  --         .. stats.count
+  --         .. " plugins"
+  --     }
+  --   }
+  --
+  --   require("alpha").setup(
+  --     startify.config
+  --   )
+  --
+  -- end,
 }
