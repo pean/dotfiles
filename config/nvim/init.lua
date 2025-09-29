b9ad5ead12c0895a2d433a2ca894c2899acdc9bc
Peter Andersson <pean@wa.se>
2025-09-29 09:23:18 +0200

    chore: add comments on options to explain what they do

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,19 +10,19 @@ vim.opt.clipboard = "unnamed" -- interact with system clipboard
 vim.opt.colorcolumn = { 88 }
 
 -- Indentation settings (Treesitter handles smart indentation)
-vim.opt.expandtab = true -- use spaces instead of tabs
-vim.opt.tabstop = 2 -- tab width
-vim.opt.softtabstop = 2 -- number of spaces tab counts for while editing
-vim.opt.shiftwidth = 2 -- indentation level
+vim.opt.expandtab = true  -- use spaces instead of tabs
+vim.opt.tabstop = 2       -- tab width
+vim.opt.softtabstop = 2   -- number of spaces tab counts for while editing
+vim.opt.shiftwidth = 2    -- indentation level
 vim.opt.autoindent = true -- copy indent from current line when starting new line
 vim.opt.shiftround = true -- round indent to multiple of shiftwidth
 -- Note: smartindent and cindent disabled to avoid conflicts with Treesitter
 
-vim.opt.list = true -- show whitespace and some special characters
+vim.opt.list = true   -- show whitespace and some special characters
 vim.opt.listchars = { tab = ">-", trail = "-", extends = ">" }
 vim.opt.number = true -- show line numbers
 vim.opt.whichwrap:append("<,>,h,l,[,]")
-vim.opt.wrap = false -- do not wrap lines
+vim.opt.wrap = false  -- do not wrap lines
 
 vim.api.nvim_set_hl(0, "ColorColumn", { link = "StatusLine" })
 
