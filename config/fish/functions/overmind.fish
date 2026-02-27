61c4d1ae31e36957b6d0c423b332096cdef0c879
Peter Andersson <pean@wa.se>
2026-02-27 08:12:00 +0100

    feat: look for env file by traversing upwards

--- config/fish/functions/overmind.fish
+++ config/fish/functions/overmind.fish
@@ -2,6 +2,7 @@ function overmind --description 'Overmind wrapper that uses Procfile.dev for sta
     set -l procfile_dev "Procfile.dev"
     set -l has_f_flag 0
     set -l is_start_command 0
+    set -l env_file ""
 
     # Check if first argument is "start" (or "s")
     if test (count $argv) -gt 0
@@ -15,6 +16,36 @@ function overmind --description 'Overmind wrapper that uses Procfile.dev for sta
         if test "$arg" = "-f" -o "$arg" = "--procfile"
             set has_f_flag 1
             break
+        end
+    end
+
+    # Look for .overmind.env by traversing upward until we find it or hit a .git directory
+    set env_file ""
+    set search_dir (pwd)
+
+    while test "$search_dir" != "/"
+        if test -f $search_dir/.overmind.env
+            set env_file $search_dir/.overmind.env
+            break
+        end
+
+        # Stop if we hit a .git directory (bare repo root)
+        if test -d $search_dir/.git
+            break
+        end
+
+        # Move up one directory
+        set search_dir (dirname $search_dir)
+    end
+
+    # Load environment variables from .overmind.env if found
+    if test -n "$env_file"
+        for line in (cat $env_file)
+            # Skip empty lines and comments
+            if test -n "$line" -a (string sub -l 1 -- $line) != "#"
+                # Export the variable
+                set -gx (string split -m 1 "=" -- $line)
+            end
         end
     end
 
