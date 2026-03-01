9c9cb15de89303355847839ba3967101d370839a
Peter Andersson <pean@wa.se>
2026-03-01 08:25:08 +0100

    chore: replace tw, t etc with twine plugin

--- config/fish/functions/muxdev.fish
+++
@@ -1,9 +0,0 @@
-function muxdev
-  if git rev-parse --git-dir >/dev/null 2>&1
-    set reponame (basename (git rev-parse --show-toplevel) | sed 's/^\.//')
-    echo "Starting tmuxinator for $reponame in $pwd"
-    tmuxinator start -n $reponame dev $argv
-  else
-    echo "Not a repo"
-  end
-end
